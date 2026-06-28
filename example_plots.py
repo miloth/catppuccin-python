9fe79cd2e89c6aab644809b58b90f073e6289a34
miloth <miloth@users.noreply.github.com>
2026-06-28 09:20:46 +0100

    feat: example loads styles from module root

--- example_plots.py
+++ example_plots.py
@@ -156,7 +156,7 @@ example_plots = {
 
 if __name__ == "__main__":
     palette_name = "mocha"
-    mpl.style.use(f"catppuccin.extras.matplotlib_styles.{palette_name}")
+    mpl.style.use(f"catppuccin.{palette_name}")
     plot_palette(palette_name)
     example_plot()
     example_scatter()
@@ -164,5 +164,4 @@ if __name__ == "__main__":
     example_bar()
     example_patches()
     example_imshow()
-    plot_examples(list(asdict(PALETTE).keys()))
     plt.show()
